6a66910ac59a72e1a94271c0f2730d356f11a1ff
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-12-01 16:51:42 +0000

    Fix issue in server.js

--- server.js
+++ server.js
@@ -20,11 +20,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set a static folder
   app.use(express.static('client/build'));
 
-  app.get(
-    '*',
-    (req,
-    (res) =>
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
